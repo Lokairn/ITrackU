be542968e1edc6fadc5193306d0f2009f80c4acf
Kahinh <39779996+Kahinh@users.noreply.github.com>
2018-07-01 18:53:08 +0200

    Debuffs_table for dungeon and Stun module

---
+++ tables/BFA/dungeons/ShrineOfTheStorm/debuffs_table.lua
@@ -0,0 +1,46 @@
+﻿  if debuffs_table == nil then debuffs_table = {} end
+
+-- BDD of dungeons difficulty
+-- ITrack.dungeons_difficulty = {
+  -- [1] = "Normal",
+  -- [2] = "Heroic",
+  -- [23] = "Mythic",
+  -- [8] = "Mythic +",
+  
+  --Alunza
+    if debuffs_table[2084] == nil then
+      debuffs_table[2084] = {
+        -- Normal
+        [1] = {
+          -- Corruption Sanguine
+          [255558] = {
+            ["IfActive"] = false,
+            ["PlayerOnly"] = "All",
+            ["Count"] = 0,
+            ["Type"] = "Classic",
+            ["TypeDistance"] = 0,
+            ["Rôle"] = "All",
+            ["MaxStacks"] = true,
+            ["MaxStacksNumber"] = 5,
+            ["Columns"] = 1,
+            ["Activate"] = true,
+          },
+        -- Or en fusion
+          [255584] = {
+            ["IfActive"] = false,
+            ["PlayerOnly"] = "All",
+            ["Count"] = 0,
+            ["Type"] = "Classic",
+            ["TypeDistance"] = 0,
+            ["Rôle"] = "All",
+            ["MaxStacks"] = true,
+            ["MaxStacksNumber"] = 5,
+            ["Columns"] = 1,
+            ["Activate"] = true,
+          },
+        },
+      }
+    end
+
+  --Rezan
+      
